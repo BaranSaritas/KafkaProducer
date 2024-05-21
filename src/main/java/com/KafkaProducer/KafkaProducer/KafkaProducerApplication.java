91a237222aca9bb9ba3a3b0cc1a1ef606000cb70
Baran Sarıtaş <baransaritas.developer@gmail.com>
2024-05-21 17:43:37 +0300

    Consumer okuma ve deserializer sorunu giderildi

--- src/main/java/com/KafkaProducer/KafkaProducer/KafkaProducerApplication.java
+++ src/main/java/com/KafkaProducer/KafkaProducer/KafkaProducerApplication.java
@@ -2,10 +2,16 @@ package com.KafkaProducer.KafkaProducer;
 
 import com.KafkaProducer.KafkaProducer.dto.SModel;
 
+import io.confluent.kafka.serializers.KafkaJsonDeserializer;
 import io.confluent.kafka.serializers.KafkaJsonSerializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +20,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.core.KafkaTemplate;
 
+import java.time.Duration;
+import java.util.Arrays;
 import java.util.Properties;
 
 
@@ -33,6 +41,10 @@ public class KafkaProducerApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		//http://localhost:29092/
+
+		// Producer senaryolari
+		/*
+
 		Properties config = new Properties();
 		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"http://localhost:29092");
 		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
@@ -48,7 +60,7 @@ public class KafkaProducerApplication implements CommandLineRunner {
 		producer.flush();
 
 
-
+*/
 		/*for (int i = 0; i < 6; i++) {
 			int key = i % 6;
 			JSONObject data = new JSONObject();
@@ -58,6 +70,26 @@ public class KafkaProducerApplication implements CommandLineRunner {
 			kafkaTemplate.send("Jsons",key,String.valueOf(key),data.toString());
 			kafkaTemplate.flush();
 		}*/
+
+		// Consumer senaryolari
+
+		Properties config = new Properties();
+		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+		config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		config.put(ConsumerConfig.GROUP_ID_CONFIG, "kafka_group_deneme");
+		config.put(ConsumerConfig.CLIENT_ID_CONFIG, "kafka_client_deneme");
+		config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(config);
+		consumer.subscribe(Arrays.asList("serializer"));
+
+		ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMinutes(1));
+
+		for (ConsumerRecord<String, String> record : consumerRecords) {
+			System.out.println(record.value());
+		}
+
 
 
 
